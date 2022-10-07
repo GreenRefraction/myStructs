c4807279e7cb3bbde4fd708d36e5b798729510e1
GreenRefraction <axel.gronland@gmail.com>
2022-10-07 12:14:53 +0200

    bugfixes and added variable size

--- maxheap.py
+++ maxheap.py
@@ -1,51 +1,63 @@
 import math
 import sys
+
  
 class MaxHeap:
- 
-    def __init__(self, maxsize, key=None):
-        self.maxsize = maxsize
+    def __init__(self, iter=None, key=None):
         self.size = 0
         if key is None:
             key = lambda i: i
         self.key = key
-        self.arr = [None] * (self.maxsize + 1)
- 
+        self.arr:list = [None]
+        if iter is not None:
+            for i in iter:
+                self.push(i)
+    
     def swap(self, fpos, spos):
         self.arr[fpos], self.arr[spos] = (self.arr[spos], self.arr[fpos])
 
     def _sift_up(self, k):
         while k > 1:
-            if self.key(self.arr[k]) > self.key(self.arr[k >> 1]):
-                self.swap(k, k >> 1)
-            k >>= 1
-    
-    def _sift_down(self, curr):
-        parent = curr >> 1
-        if parent == 0: return
-        self._sift_down(parent)
-        if self.key(self.arr[curr]) > self.key(self.arr[parent]):
-            self.swap(curr, parent)
-        if curr^1 <= self.size and self.key(self.arr[curr^1]) > self.key(self.arr[parent]):
-            self.swap(curr^1, parent)
-        
+            parent = k >> 1
+            if self.key(self.arr[k]) > self.key(self.arr[parent]):
+                self.swap(k, parent)
+            else:
+                break
+            k = parent
+
+    def _sift_down(self):
+        k = 1
+        while k <= self.size:
+            left = k << 1
+            right = left + 1
+            parent = k >> 1
+            if parent > 0 and self.key(self.arr[parent]) < self.key(self.arr[k]):
+                self.swap(parent, k)
+            if right <= self.size:
+                if self.key(self.arr[left]) > self.key(self.arr[right]):
+                    k = left
+                else:
+                    k = right
+            else:
+                k = left
 
     def push(self, element):
         self.size += 1
-        self.arr[self.size] = element
+        self.arr.append(element)
         self._sift_up(self.size)
  
     def pop(self):
-        if self.size == 1:
-            popped = self.arr[1]
-            self.arr[1] = None
+        if self.size == 0:
+            return None
+        elif self.size == 1:
+            popped = self.arr.pop()
             self.size = 0
         else:
             popped = self.arr[1]
             self.arr[1] = self.arr[self.size]
-            self.arr[self.size] = None
+            self.arr.pop()
             self.size -= 1
-            self._sift_down(self.size)
+            self._sift_down()
         return popped
     
     def top(self):
@@ -57,7 +69,8 @@ class MaxHeap:
         out = ""
         curr_layer = [1]
         for k in range(int(math.log2(self.size)) + 1):
-            out += " ".join(map(lambda i: str(self.arr[i]) if i <= self.size else "-", curr_layer)) + "\n"
+            out += " ".join(map(lambda i: str(self.key(self.arr[i])) if i <= self.size else "-", curr_layer)) + "\n"
+            #out += " ".join(map(lambda i: str(self.arr[i]) if i <= self.size else "-", curr_layer)) + "\n"
             next_layer = list()
             for i in curr_layer:
                 left = i << 1
@@ -68,13 +81,21 @@ class MaxHeap:
         return out
     def __len__(self):
         return self.size
+    
+    def _check_health(self, i=1):
+        
+        left = i << 1
+        right = left + 1
+        if left <= self.size:
+            assert self.key(self.arr[i]) >= self.key(self.arr[left])
+            self._check_health(left)
+        if right <= self.size:
+            assert self.key(self.arr[i]) >= self.key(self.arr[right])
+            self._check_health(right)
 # Driver Code
-if __name__ == "__main__":    
-    heap = MaxHeap(15)
-    N = 10
-    for i in range(N):
-        heap.push(i)
+if __name__ == "__main__":
+    N = 20
+    heap = MaxHeap(range(N))
+    print(heap)
     for i in range(N):
         print(heap.pop())
-    
-    
